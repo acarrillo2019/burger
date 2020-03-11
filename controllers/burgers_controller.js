b0f61cc2ab06ec5d3f4bd9b9c1d1f298531a26b4
acarrillo2019 <alric21@aol.com>
2020-03-10 17:57:31 -0700

    Finished controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -31,3 +31,19 @@ router.get("/", function(req, res) {
       }
     });
   });
+
+  router.delete("/api/burgers/:id", function(req, res){
+    var condition = `id = ${req.params.id}`;
+  
+    // Delete burger entry in db
+    burger.delete(condition, function(result){
+      if (result.affectedRows == 0){
+        return res.status(404).end();
+      }
+      else{
+        res.status(200).end();
+      }
+    })
+  })
+  
+  module.exports = router;
