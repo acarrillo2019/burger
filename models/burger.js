efa695411ead15a747dfd632bae5a35f9d7c00ce
acarrillo2019 <alric21@aol.com>
2020-03-11 22:33:56 -0700

    Finished burger js

--- models/burger.js
+++ models/burger.js
@@ -16,3 +16,11 @@ var burger = {
           cb(res);
         });
       },
+      delete: function(condition, cb){
+        orm.delete("burgers", condition, function(res){
+          cb(res);
+        });
+      }
+    };
+    
+    module.exports = burger;
