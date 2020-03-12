90f2ef960c62308c09281e3aeb0ac2f4653f03ff
acarrillo2019 <alric21@aol.com>
2020-03-11 22:31:56 -0700

    Added to burger js

--- models/burger.js
+++ models/burger.js
@@ -6,3 +6,8 @@ var burger = {
         cb(res);
       });
     },
+    create: function(cols, vals, cb){
+        orm.create("burgers", cols, vals, function(res){
+          cb(res);
+        });
+      },
