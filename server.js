815bf7003118565a3be4af67d1426b6acb005bcd
acarrillo2019 <alric21@aol.com>
2020-03-13 19:26:32 -0700

    Added to server.js

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller");
+
+// Setup the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Setup the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Setup Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
