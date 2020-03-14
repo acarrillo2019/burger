1119ba41d27192d8081aee484ee6eda456fec239
acarrillo2019 <alric21@aol.com>
2020-03-13 22:52:01 -0700

    Styling

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
 // Setup connection to database
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-};
 
-connection.connect(function(err) {
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -21,4 +19,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
