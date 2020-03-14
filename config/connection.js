f35d2253901b0d14c487da3dc64dbc8a243e8ab6
acarrillo2019 <alric21@aol.com>
2020-03-14 10:19:23 -0700

    Added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,17 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+};
+
 // Make connection.
 connection.connect(function (err) {
   if (err) {
