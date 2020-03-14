81903b2d7d6907581766eb0bf4d3e7de4f8cf520
acarrillo2019 <alric21@aol.com>
2020-03-14 04:55:23 -0700

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var exphbs = require("express-handlebars");
 
 // Setup the Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Setup the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
