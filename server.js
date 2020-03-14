344d59cdf7d07102dfcbff56369f4b72f5e55d41
acarrillo2019 <alric21@aol.com>
2020-03-13 19:25:40 -0700

    Added dependancies

---
+++ server.js
@@ -0,0 +1,6 @@
+// Dependencies
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller");
