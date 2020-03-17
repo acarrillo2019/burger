221b2e726bf741b7ca2d875b01ce1cec98f92f46
acarrillo2019 <alric21@aol.com>
2020-03-16 22:30:33 -0700

    Removed node modules

--- db/schema.sql
+++ db/schema.sql
@@ -7,9 +7,17 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 id INT NOT NULL AUTO_INCREMENT,
-burger_name VARCHAR (20) NOT NULL,
+burger_name VARCHAR (255) NOT NULL,
 devoured BOOLEAN DEFAULT false,
 PRIMARY KEY (id)
 );
 
 SELECT * FROM burgers
+
+CREATE TABLE burger (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR (255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY(id)
+);
+SELECT * FROM burger
